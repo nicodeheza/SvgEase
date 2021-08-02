2d6ebf42d15ce647599e6219b70da8a0689fcc47
nicodeheza <nicolasdeheza@hotmail.com>
2021-08-02 13:12:52 -0300

    css cuadricula galeria

--- components/Search.js
+++ components/Search.js
@@ -54,6 +54,7 @@ export default function Search({setFloatWin, open, categories, searchQuery}){
         }
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     },[query]);
 
     
