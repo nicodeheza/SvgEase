28f2c00f9c9fb0ec0493bdbf421eb051bccb854a
nicodeheza <nicolasdeheza@hotmail.com>
2021-08-19 16:43:41 -0300

    cabio en busqueda de or por in

--- helpersFunctions/getDbQuery.js
+++ helpersFunctions/getDbQuery.js
@@ -2,39 +2,23 @@ export default function getDbQuery(searchQuery){
 
     if(searchQuery.categories.length > 0 ||  searchQuery.tags.length > 0 || searchQuery.text){
 
-        let obj= {$or:[]};
-        if(searchQuery.categories.length === 1){
-            obj.category= searchQuery.categories[0];
-        }else{
-            searchQuery.categories.forEach(ele=>{
-                obj.$or.push({category: ele});
-            });
+        let obj= {};
+        if(searchQuery.categories.length > 0){
+            obj.category= {$in: searchQuery.categories};
         }
-        if(searchQuery.tags.length === 1){
-            obj.tags= searchQuery.tags[0];
-        }else{
-            searchQuery.tags.forEach(ele=>{
-                obj.$or.push({tags: ele});
-            });
+        if(searchQuery.tags.length > 0){
+            obj.tags= {$in: searchQuery.tags};
         }
         if(searchQuery.text){
             const regEx= new RegExp(searchQuery.text, 'i');
-            obj.$or.push({name: regEx});
-            obj.$or.push({tags: regEx});
-            obj.$or.push({category: regEx});
+            obj.name= regEx,
+            obj.category?.$in.push(regEx);
+            obj.tags?.$in.push(regEx);
         }
 
-        if(obj.$or.length === 1){
-            return obj.$or[0];
-        }else{
-            if(obj.$or.length === 0){
-                delete obj.$or;
-            }
-            return obj;
-        }
+        return obj;
 
     }else{
-
         return {};
     }
 
