ca0c78c95ef64fea2930be756f69c7c4664fc89b
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-29 12:17:24 -0300

    SingupIcon change case in import

--- components/Cart.js
+++ components/Cart.js
@@ -1,220 +1,241 @@
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import Close from "./icons/Close";
 import LoginIcon from "./icons/LoginIcon";
-import SingupIcon from "./icons/singupIcon";
-import styles from './cart.module.css';
+import SingupIcon from "./icons/SingupIcon";
+import styles from "./cart.module.css";
 import priceInPesos from "../helpersFunctions/priceInPesos";
 import CartPreview from "./CartPreview";
-import DropIcon from './icons/DropIcon';
+import DropIcon from "./icons/DropIcon";
 import Lottie from "lottie-web";
-import { useAuthContext } from "../contexts/authContext";
-import Image from 'next/image';
-import {useRouter} from 'next/router'
+import {useAuthContext} from "../contexts/authContext";
+import Image from "next/image";
+import {useRouter} from "next/router";
 import PayPal from "./PayPal";
 
-export default function Cart({setFloatWin, open, store, currency, updateCart, 
-    setUpdateCart, usaToArs, cartProducts, setCartProducts, userProducts }){
-    const {auth}= useAuthContext();
-    const [total, setTotal]=useState(0);
-    const [repeat, setRepeat]= useState();
-    const router= useRouter();
+export default function Cart({
+	setFloatWin,
+	open,
+	store,
+	currency,
+	updateCart,
+	setUpdateCart,
+	usaToArs,
+	cartProducts,
+	setCartProducts,
+	userProducts
+}) {
+	const {auth} = useAuthContext();
+	const [total, setTotal] = useState(0);
+	const [repeat, setRepeat] = useState();
+	const router = useRouter();
 
-    useEffect(()=>{
-        if(updateCart){
+	useEffect(() => {
+		if (updateCart) {
+			const localStorage = window.localStorage;
 
-        const localStorage= window.localStorage;
+			if (localStorage.getItem("cart")) {
+				const cart = JSON.parse(localStorage.getItem("cart"));
+				setCartProducts(cart);
+			}
+			setUpdateCart(false);
+		}
+	}, [updateCart, setUpdateCart, setCartProducts]);
 
-        if(localStorage.getItem('cart')){
+	useEffect(() => {
+		for (let i = 0; i < cartProducts.length; i++) {
+			const ele = cartProducts[i];
+			if (userProducts?.includes(ele._id)) {
+				setRepeat(true);
+				break;
+			} else if (i === cartProducts.length - 1) {
+				setRepeat(false);
+				break;
+			}
+		}
+	}, [cartProducts, userProducts]);
 
-            const cart= JSON.parse(localStorage.getItem('cart'));
-            setCartProducts(cart);
-        }
-            setUpdateCart(false);
-        }
-    },[updateCart, setUpdateCart, setCartProducts]);
+	useEffect(() => {
+		let total = 0;
+		cartProducts.forEach((ele) => {
+			total += ele.price;
+		});
+		if (currency === "ars") {
+			total = priceInPesos(total, usaToArs);
+		}
+		//console.log('getTotal');
+		setTotal(Math.round(total * 100) / 100);
+	}, [currency, cartProducts, usaToArs]);
 
-    useEffect(()=>{
-        for(let i= 0; i < cartProducts.length; i++){
-            const ele= cartProducts[i];
-            if(userProducts?.includes(ele._id)){
-                setRepeat(true);
-                break;
-            }else if( i === cartProducts.length -1){
-                setRepeat(false);
-                break;
-            }
-        }
-    },[cartProducts,userProducts]);
+	function deleteProduct(id) {
+		const localStorage = window.localStorage;
+		const cart = JSON.parse(localStorage.getItem("cart"));
+		const newCart = cart.filter((ele) => ele._id !== id);
+		localStorage.setItem("cart", JSON.stringify(newCart));
+		Lottie.destroy(`cartPreview${id}`);
+		setUpdateCart(true);
+	}
 
-    useEffect(() => {
-        let total= 0;
-        cartProducts.forEach(ele=>{
-            total += ele.price
-        });
-        if(currency === 'ars'){
-            total= priceInPesos( total, usaToArs);
-        }
-        //console.log('getTotal');
-       setTotal(Math.round(total * 100) / 100);
-        
-    }, [currency, cartProducts, usaToArs]);
+	function clearCart() {
+		const localStorage = window.localStorage;
+		const cart = JSON.parse(localStorage.getItem("cart"));
+		localStorage.setItem("cart", JSON.stringify([]));
+		cart.forEach((ele) => {
+			Lottie.destroy(`cartPreview${ele._id}`);
+		});
+		setUpdateCart(true);
+	}
 
+	function mpPay() {
+		const mp = new MercadoPago("TEST-b6e0406f-2696-481b-b367-1ac83e93b342", {
+			locale: "es-AR"
+		});
 
-    function deleteProduct(id){
-        const localStorage= window.localStorage;
-        const cart= JSON.parse(localStorage.getItem('cart'));
-        const newCart= cart.filter(ele=> ele._id !== id);
-        localStorage.setItem('cart', JSON.stringify(newCart));
-        Lottie.destroy(`cartPreview${id}`);
-        setUpdateCart(true);
-    }
+		let products = [];
+		cartProducts.forEach((ele) => {
+			const obj = {
+				id: ele._id,
+				title: ele.name,
+				unit_price: priceInPesos(ele.price, usaToArs),
+				quantity: 1
+			};
+			products.push(obj);
+		});
 
-    function clearCart(){
-        const localStorage= window.localStorage;
-        const cart= JSON.parse(localStorage.getItem('cart'));
-        localStorage.setItem('cart', JSON.stringify([]));
-        cart.forEach(ele=>{
-            Lottie.destroy(`cartPreview${ele._id}`);
-        });
-        setUpdateCart(true);
-    }
+		fetch("/api/mp/create_preference", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({products})
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				mp.checkout({
+					preference: {id: data.id},
+					autoOpen: true
+				});
 
-    function mpPay(){
-        const mp= new MercadoPago('TEST-b6e0406f-2696-481b-b367-1ac83e93b342', {locale: 'es-AR'});
+				// console.log(data);
+				setFloatWin("none");
+				clearCart();
+			})
+			.catch((err) => console.log(err));
+	}
 
-        let products= [];
-        cartProducts.forEach(ele=>{
-            const obj={
-                id: ele._id,
-                title: ele.name,
-                unit_price: priceInPesos(ele.price, usaToArs),
-                quantity: 1
-            }
-            products.push(obj);
-        });
-
-        fetch('/api/mp/create_preference',{
-            method: 'POST',
-            headers:{
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({products})
-        })
-        .then(res=> res.json())
-        .then(data=>{
-            mp.checkout({
-                preference:{id: data.id},
-                autoOpen: true
-            });
-            
-           // console.log(data);
-           setFloatWin('none');
-           clearCart();
-        })
-        .catch(err=> console.log(err));
-    }
-
-
-
-    return(
-        <aside className={open && store ? styles.storeCartMainContainer : !open && store ? styles.storeCartMainContainerClose :
-             open && !store ? styles.cartMainContainer : styles.cartMainContainerClose}>
-            <div className={styles.headerContainer}>
-                <h3>Carro de Compras</h3>
-                <div onClick={()=>setFloatWin('none')} className={styles.closeContainer}>
-                    <Close/>
-                </div>
-            </div>
-            <table className={styles.table}>
-                <thead>
-                <tr>
-                <th>Producto</th>
-                <th>Precio</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    cartProducts.map((product, i)=>(
-                        <tr key={i} className={userProducts?.includes(product._id) ? styles.productRepeat : styles.productsTr}>
-                        <td className={styles.priceTitle}>
-                            <div className={styles.preview}>
-                                <CartPreview file={product.file} id={product._id}/>
-                            </div>
-                            <h5>{product.name}</h5>
-                        </td>
-                        <td className={styles.priceContainer}>
-                            <h5>{`${currency === 'ars' ? priceInPesos(product.price, usaToArs) : product.price}$ ${currency.toUpperCase()}`}</h5>
-                        </td>
-                        <td>
-                            <div onClick={()=>deleteProduct(product._id)}>
-                            <DropIcon  classN={styles.dropIcon}/>
-                            </div>
-                        </td>
-                        </tr>
-                    ))
-                      
-                }
-                </tbody>
-                <tfoot>
-                <tr>
-                <th>Total:</th>
-                <td className={styles.totalPrice}>{`${total}$ ${currency.toUpperCase()}`}</td>
-                </tr>
-                </tfoot>
-            </table>
-            {
-                !auth ? 
-                (
-            <div className={styles.btnContainer}>
-                <button onClick={()=> setFloatWin('logIn')}>
-                    Iniciar Sesión <span><LoginIcon classN={styles.iconBtn}/></span>
-                </button>
-                <button onClick={()=> setFloatWin('singUp')}>
-                    Crear Cuenta <span><SingupIcon classN={styles.iconBtn}/></span>
-                </button>
-            </div>
-                ) 
-                : total <= 0 ? 
-                (
-                    <div className={styles.btnContainer}>
-                        <p style={{fontFamily:'var(--text-font)',
-                    color:'red'}}>Carro vacío</p>
-                    </div>
-                )
-                : repeat ? 
-                (
-                    <div className={styles.btnContainer}>
-                        <p style={{fontFamily:'var(--text-font)',
-                    color:'red',
-                    width:'80%'}}>Ya tienes uno o mas productos en tu cuenta, quita los del carro para poder pagar.</p>
-                    </div>
-
-                ) 
-                :
-                (
-                    <div className={styles.payBtnContainer}>
-                        {
-                            currency === 'ars' ? (
-                                <>
-                        <Image src='/svgs/mercadoPago.svg' alt='mercado pago' width={60} height={60} />
-                        <button onClick={()=>mpPay()}>
-                            Pagar
-                        </button>
-                                </>
-                            ) : (
-                                <>
-                                <PayPal 
-                                cartProducts={cartProducts}
-                                setFloatWin={setFloatWin}
-                                clearCart={clearCart}
-                                router={router}/>
-                                </>
-                            )
-                        }
-                    </div>
-                ) 
-            }
-        </aside>
-    )
+	return (
+		<aside
+			className={
+				open && store
+					? styles.storeCartMainContainer
+					: !open && store
+					? styles.storeCartMainContainerClose
+					: open && !store
+					? styles.cartMainContainer
+					: styles.cartMainContainerClose
+			}
+		>
+			<div className={styles.headerContainer}>
+				<h3>Carro de Compras</h3>
+				<div onClick={() => setFloatWin("none")} className={styles.closeContainer}>
+					<Close />
+				</div>
+			</div>
+			<table className={styles.table}>
+				<thead>
+					<tr>
+						<th>Producto</th>
+						<th>Precio</th>
+					</tr>
+				</thead>
+				<tbody>
+					{cartProducts.map((product, i) => (
+						<tr
+							key={i}
+							className={
+								userProducts?.includes(product._id)
+									? styles.productRepeat
+									: styles.productsTr
+							}
+						>
+							<td className={styles.priceTitle}>
+								<div className={styles.preview}>
+									<CartPreview file={product.file} id={product._id} />
+								</div>
+								<h5>{product.name}</h5>
+							</td>
+							<td className={styles.priceContainer}>
+								<h5>{`${
+									currency === "ars"
+										? priceInPesos(product.price, usaToArs)
+										: product.price
+								}$ ${currency.toUpperCase()}`}</h5>
+							</td>
+							<td>
+								<div onClick={() => deleteProduct(product._id)}>
+									<DropIcon classN={styles.dropIcon} />
+								</div>
+							</td>
+						</tr>
+					))}
+				</tbody>
+				<tfoot>
+					<tr>
+						<th>Total:</th>
+						<td className={styles.totalPrice}>{`${total}$ ${currency.toUpperCase()}`}</td>
+					</tr>
+				</tfoot>
+			</table>
+			{!auth ? (
+				<div className={styles.btnContainer}>
+					<button onClick={() => setFloatWin("logIn")}>
+						Iniciar Sesión{" "}
+						<span>
+							<LoginIcon classN={styles.iconBtn} />
+						</span>
+					</button>
+					<button onClick={() => setFloatWin("singUp")}>
+						Crear Cuenta{" "}
+						<span>
+							<SingupIcon classN={styles.iconBtn} />
+						</span>
+					</button>
+				</div>
+			) : total <= 0 ? (
+				<div className={styles.btnContainer}>
+					<p style={{fontFamily: "var(--text-font)", color: "red"}}>Carro vacío</p>
+				</div>
+			) : repeat ? (
+				<div className={styles.btnContainer}>
+					<p style={{fontFamily: "var(--text-font)", color: "red", width: "80%"}}>
+						Ya tienes uno o mas productos en tu cuenta, quita los del carro para poder
+						pagar.
+					</p>
+				</div>
+			) : (
+				<div className={styles.payBtnContainer}>
+					{currency === "ars" ? (
+						<>
+							<Image
+								src="/svgs/mercadoPago.svg"
+								alt="mercado pago"
+								width={60}
+								height={60}
+							/>
+							<button onClick={() => mpPay()}>Pagar</button>
+						</>
+					) : (
+						<>
+							<PayPal
+								cartProducts={cartProducts}
+								setFloatWin={setFloatWin}
+								clearCart={clearCart}
+								router={router}
+							/>
+						</>
+					)}
+				</div>
+			)}
+		</aside>
+	);
 }
--- components/btns/TextBtn.js
+++ components/btns/TextBtn.js
@@ -1,16 +1,21 @@
-import LoginIcon from '../icons/LoginIcon'
-import SingupIcon from '../icons/singupIcon'
-import style from './textBtn.module.css'
+import LoginIcon from "../icons/LoginIcon";
+import SingupIcon from "../icons/SingupIcon";
+import style from "./textBtn.module.css";
 
-export default function TextBtn({text, icon, setFloatWin, target, movil, store}){
-     
-    return(
-        <div onClick={()=> setFloatWin(target)} className={store && movil ? style.containerMovilStore : 
-        movil ? style.containerMovil : style.container}>
-            <p className={style.text}>{text}</p>
-            {icon ==='login' ?
-            (<LoginIcon />) : (<SingupIcon />)}
-            
-        </div>
-    )
+export default function TextBtn({text, icon, setFloatWin, target, movil, store}) {
+	return (
+		<div
+			onClick={() => setFloatWin(target)}
+			className={
+				store && movil
+					? style.containerMovilStore
+					: movil
+					? style.containerMovil
+					: style.container
+			}
+		>
+			<p className={style.text}>{text}</p>
+			{icon === "login" ? <LoginIcon /> : <SingupIcon />}
+		</div>
+	);
 }
