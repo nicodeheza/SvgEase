5d3fe88d3e18fe9f0e4e399194fa54403000b5e9
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-29 12:12:03 -0300

    ProductCard change case in import

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,133 +1,138 @@
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react";
 import Lottie from "lottie-web";
-import styles from './productCard.module.css';
+import styles from "./productCard.module.css";
 import priceInPesos from "../helpersFunctions/priceInPesos";
 
+export default function ProductCard({
+	product,
+	currency,
+	store,
+	usaToArs,
+	setEdit,
+	edit,
+	setPreviewProduct,
+	setUpdateCart,
+	cartProducts
+}) {
+	const [activeBtn, setActiveBtn] = useState(false);
 
-export default function ProductCard({product, currency, store, usaToArs, setEdit, 
-    edit, setPreviewProduct, setUpdateCart, cartProducts }){
+	useEffect(() => {
+		Lottie.destroy(product._id);
+		const animation = product.file;
+		if (animation) {
+			Lottie.loadAnimation({
+				container: document.getElementById(`cardAnimation${product._id}`),
+				renderer: "svg",
+				loop: true,
+				autoplay: true,
+				name: product._id,
+				animationData: animation
+			});
+		}
+	}, [product]);
 
-   const [activeBtn, setActiveBtn]= useState(false);
-   
+	useEffect(() => {
+		//product selected update
+		if (store) {
+			const localStorage = window.localStorage;
+			const cart = JSON.parse(localStorage.getItem("cart"));
+			if (cart) {
+				if (cart.find((ele) => ele._id === product._id) !== undefined) {
+					setActiveBtn(true);
+				}
+			}
+		}
+	}, [product, store]);
 
-    useEffect(()=>{
-        Lottie.destroy(product._id);
-        const animation= product.file;
-        if(animation){
-            Lottie.loadAnimation({
-                container: document.getElementById(`cardAnimation${product._id}`),
-                renderer: 'svg',
-                loop: true,
-                autoplay: true,
-                name: product._id,
-                animationData: animation
-            });
-          }
+	useEffect(() => {
+		if (store) {
+			const ele = cartProducts.find((ele) => ele._id === product._id);
+			if (ele === undefined) {
+				setActiveBtn(false);
+			} else {
+				setActiveBtn(true);
+			}
+			// console.log(ele);
+		}
+	}, [cartProducts, product, store]);
 
-    },[product]);
+	function btnClick() {
+		setActiveBtn((prev) => !prev);
+		if (!store) {
+			setEdit(product);
+		} else {
+			const localStorage = window.localStorage;
+			if (!activeBtn) {
+				const productToAdd = {
+					name: product.name,
+					price: product.price,
+					file: product.file,
+					_id: product._id
+				};
 
-    useEffect(()=>{
-   
-          //product selected update
-          if(store){
-          const localStorage= window.localStorage;
-          const cart= JSON.parse(localStorage.getItem('cart'));
-          if(cart){
-              if(cart.find(ele=> ele._id === product._id) !== undefined){
-                  setActiveBtn(true);
-              }
-          }
-         }
+				if (localStorage.getItem("cart")) {
+					const cart = JSON.parse(localStorage.getItem("cart"));
+					cart.push(productToAdd);
+					localStorage.setItem("cart", JSON.stringify(cart));
+				} else {
+					const cart = [productToAdd];
+					localStorage.setItem("cart", JSON.stringify(cart));
+				}
+			} else {
+				const cart = JSON.parse(localStorage.getItem("cart"));
+				localStorage.setItem(
+					"cart",
+					JSON.stringify(cart.filter((ele) => ele._id !== product._id))
+				);
+			}
+			setUpdateCart(true);
+			// console.log(JSON.parse(localStorage.getItem('cart')).length);
+		}
+	}
 
-    },[product, store]);
-
-    useEffect(()=>{
-        if(store){
-        const ele= cartProducts.find(ele=> ele._id === product._id);
-        if(ele === undefined){
-            setActiveBtn(false);
-        }else{
-            setActiveBtn(true);
-        }
-       // console.log(ele);
-      }
-    },[cartProducts, product, store]);
-
-
-    function btnClick(){
-        setActiveBtn(prev=> !prev);
-        if(!store){
-            setEdit(product);
-        }else{
-            const localStorage= window.localStorage;
-            if(!activeBtn){
-                const productToAdd={
-                    name: product.name,
-                    price: product.price,
-                    file: product.file,
-                    _id: product._id
-                }
-
-                if(localStorage.getItem('cart')){
-                    const cart= JSON.parse(localStorage.getItem('cart'));
-                    cart.push(productToAdd);
-                    localStorage.setItem('cart', JSON.stringify(cart));
-                }else{
-                    const cart= [productToAdd];
-                    localStorage.setItem('cart', JSON.stringify(cart));
-                }
-            }else{
-                const cart= JSON.parse(localStorage.getItem('cart'));
-                localStorage.setItem('cart', JSON.stringify(cart.filter(ele=> ele._id !== product._id)));
-            }
-            setUpdateCart(true);
-           // console.log(JSON.parse(localStorage.getItem('cart')).length);
-        }
-    }
-
-
-    return(
-
-      <div className={styles.mainContainer}>
-            
-            <div className={styles.previewContainer} onClick={()=> setPreviewProduct(product)}>
-                <div id={`cardAnimation${product._id}`} className={styles.animation}/>
-            </div>
-            <h2>{product.name}</h2>
-            <div>
-                <h3>Categorías:</h3>
-                <ul>
-                    <li>{`#${product.category}, `}</li>
-                    {
-                        product.tags.map((tag,i)=>{
-                            if(i === product.tags.length -1){
-                                return(<li key={i}>{`#${tag}`}</li>)
-                            }else{
-                                return(<li key={i}>{`#${tag}, `}</li>)
-                            }
-                        })
-                    }
-                </ul>
-            </div>
-            <div className={styles.priceBtn}>
-            <p>{`${currency === 'ars' ? priceInPesos(product.price, usaToArs) : product.price}$ ${currency.toUpperCase()}`}</p>
-            {
-                store ? 
-                (
-                    <button onClick={()=> btnClick()}
-                    style={activeBtn ? {backgroundColor: '#89d3a7'} : {}}>
-                        {!activeBtn ? 'Agregar' : 'Agregado'}
-                    </button>
-                ):
-                (
-                    <button onClick={()=> btnClick()}
-                    style={edit !== undefined && product._id === edit._id ? {backgroundColor: '#89d3a7'} : {}} >
-                        Editar
-                    </button>
-                )
-            }
-            </div>
-
-            </div>
-    )
+	return (
+		<div className={styles.mainContainer}>
+			<div className={styles.previewContainer} onClick={() => setPreviewProduct(product)}>
+				<div id={`cardAnimation${product._id}`} className={styles.animation} />
+			</div>
+			<h2>{product.name}</h2>
+			<div>
+				<h3>Categorías:</h3>
+				<ul>
+					<li>{`#${product.category}, `}</li>
+					{product.tags.map((tag, i) => {
+						if (i === product.tags.length - 1) {
+							return <li key={i}>{`#${tag}`}</li>;
+						} else {
+							return <li key={i}>{`#${tag}, `}</li>;
+						}
+					})}
+				</ul>
+			</div>
+			<div className={styles.priceBtn}>
+				<p>{`${
+					currency === "ars" ? priceInPesos(product.price, usaToArs) : product.price
+				}$ ${currency.toUpperCase()}`}</p>
+				{store ? (
+					<button
+						onClick={() => btnClick()}
+						style={activeBtn ? {backgroundColor: "#89d3a7"} : {}}
+					>
+						{!activeBtn ? "Agregar" : "Agregado"}
+					</button>
+				) : (
+					<button
+						onClick={() => btnClick()}
+						style={
+							edit !== undefined && product._id === edit._id
+								? {backgroundColor: "#89d3a7"}
+								: {}
+						}
+					>
+						Editar
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
--- components/ProductGallery.js
+++ components/ProductGallery.js
@@ -1,155 +1,174 @@
-import ProductCard from "./productCard";
-import styles from './productGallery.module.css';
-import Link from 'next/link';
+import ProductCard from "./ProductCard";
+import styles from "./productGallery.module.css";
+import Link from "next/link";
 import Preview from "./Preview";
-import { useEffect, useState } from "react";
-import {useRouter} from 'next/router';
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
 
-export default function ProductGallery({products, store, currency, usaToArs, 
-    setEdit, edit, numOfDocuments, setUpdateCart, cartProducts}){
+export default function ProductGallery({
+	products,
+	store,
+	currency,
+	usaToArs,
+	setEdit,
+	edit,
+	numOfDocuments,
+	setUpdateCart,
+	cartProducts
+}) {
+	const [componentMount, setComponentMount] = useState(false);
+	const [actualPage, setActualPage] = useState(1);
+	const [previewProduct, setPreviewProduct] = useState(null);
 
-    const [componentMount, setComponentMount]=useState(false);
-    const [actualPage, setActualPage]= useState(1);
-    const [previewProduct, setPreviewProduct]= useState(null);
+	const numOfPages = Math.ceil(numOfDocuments / 12);
 
-    const numOfPages= Math.ceil(numOfDocuments /12);
-    
-    function index(){
-        
-        let arr=[];
-       
-         const first= actualPage - 2 > 0 ? actualPage -2 : actualPage -1 > 0 ? actualPage-1 : actualPage;
-         const last= actualPage +2 <= numOfPages ? actualPage+2 : actualPage+1 <= numOfPages ? actualPage+1 : actualPage;
-     
+	function index() {
+		let arr = [];
 
-        for(let i= first; i<= last; i++){
-            arr.push(i);
-        }
-        return arr;
-    }
+		const first =
+			actualPage - 2 > 0
+				? actualPage - 2
+				: actualPage - 1 > 0
+				? actualPage - 1
+				: actualPage;
+		const last =
+			actualPage + 2 <= numOfPages
+				? actualPage + 2
+				: actualPage + 1 <= numOfPages
+				? actualPage + 1
+				: actualPage;
 
-    useEffect(()=>{
-        const gallery= document.getElementById('productGallery');
-        gallery.scrollTo(0, 0);
-    },[products]);
+		for (let i = first; i <= last; i++) {
+			arr.push(i);
+		}
+		return arr;
+	}
 
-    useEffect(()=>{
-        setComponentMount(true);
+	useEffect(() => {
+		const gallery = document.getElementById("productGallery");
+		gallery.scrollTo(0, 0);
+	}, [products]);
 
-        const urlSearchParams = new URLSearchParams(window.location.search);
-        const params = Object.fromEntries(urlSearchParams.entries());
-        if(params.page){
-            if(numOfDocuments <= 12){
-                setActualPage(1)
-            }else{
-                setActualPage(parseInt(params.page));
-            }
-        }
-    },[numOfDocuments]);
+	useEffect(() => {
+		setComponentMount(true);
 
-    const router = useRouter();
+		const urlSearchParams = new URLSearchParams(window.location.search);
+		const params = Object.fromEntries(urlSearchParams.entries());
+		if (params.page) {
+			if (numOfDocuments <= 12) {
+				setActualPage(1);
+			} else {
+				setActualPage(parseInt(params.page));
+			}
+		}
+	}, [numOfDocuments]);
 
-    function changePage(operation){
-        if(componentMount){
-            const urlSearchParams = new URLSearchParams(window.location.search);
-            const params = Object.fromEntries(urlSearchParams.entries());
-            if(params.page){
-                if(operation === '+'){
-                    if(actualPage + 1 <= numOfPages){
-                        urlSearchParams.set('page', actualPage + 1);
-                        setActualPage(prev=> prev + 1);
-                    }
-                }else if(operation ==='-'){
-                    if(actualPage -1 > 0){
-                        urlSearchParams.set('page', actualPage - 1);
-                        setActualPage(prev=> prev - 1);
-                    }
-                }
-            }else{
-                if(operation === '+'){
-                    if(actualPage + 1 <= numOfPages){
-                        urlSearchParams.append('page', actualPage + 1);
-                        setActualPage(prev=> prev + 1);
-                    }
-                }else if(operation ==='-'){
-                    if(actualPage -1 > 0){
-                        urlSearchParams.append('page', actualPage - 1);
-                        setActualPage(prev=> prev - 1);
-                    }
-                }
-            }
+	const router = useRouter();
 
-            const newParam= urlSearchParams.toString()
-            if(store){
-                router.replace(`/tienda?${newParam}`);
-            }else{
-                router.replace(`/admin?${newParam}`);
-            }
-        }
-    }
+	function changePage(operation) {
+		if (componentMount) {
+			const urlSearchParams = new URLSearchParams(window.location.search);
+			const params = Object.fromEntries(urlSearchParams.entries());
+			if (params.page) {
+				if (operation === "+") {
+					if (actualPage + 1 <= numOfPages) {
+						urlSearchParams.set("page", actualPage + 1);
+						setActualPage((prev) => prev + 1);
+					}
+				} else if (operation === "-") {
+					if (actualPage - 1 > 0) {
+						urlSearchParams.set("page", actualPage - 1);
+						setActualPage((prev) => prev - 1);
+					}
+				}
+			} else {
+				if (operation === "+") {
+					if (actualPage + 1 <= numOfPages) {
+						urlSearchParams.append("page", actualPage + 1);
+						setActualPage((prev) => prev + 1);
+					}
+				} else if (operation === "-") {
+					if (actualPage - 1 > 0) {
+						urlSearchParams.append("page", actualPage - 1);
+						setActualPage((prev) => prev - 1);
+					}
+				}
+			}
 
+			const newParam = urlSearchParams.toString();
+			if (store) {
+				router.replace(`/tienda?${newParam}`);
+			} else {
+				router.replace(`/admin?${newParam}`);
+			}
+		}
+	}
 
-    return(
-        <div className={styles.container} id='productGallery'>
-        {
-            products.map((product)=>{
-                if(product._id){
-                    return(
-                    <ProductCard product={product} currency={currency} 
-                    store={store} key={product._id} usaToArs={usaToArs}
-                    setEdit={setEdit} edit={edit} setPreviewProduct={setPreviewProduct} 
-                    setUpdateCart={setUpdateCart} cartProducts={cartProducts}/>
-                    )
-                }
-            }
-            )
-        }
+	return (
+		<div className={styles.container} id="productGallery">
+			{products.map((product) => {
+				if (product._id) {
+					return (
+						<ProductCard
+							product={product}
+							currency={currency}
+							store={store}
+							key={product._id}
+							usaToArs={usaToArs}
+							setEdit={setEdit}
+							edit={edit}
+							setPreviewProduct={setPreviewProduct}
+							setUpdateCart={setUpdateCart}
+							cartProducts={cartProducts}
+						/>
+					);
+				}
+			})}
 
-       <ul className={styles.index}>
-           <li onClick={()=> changePage('-')} >{'<'}</li>
-           {
-             index().map(ele=>{
-                 if(componentMount){
-                     const urlSearchParams = new URLSearchParams(window.location.search);
-                     const params = Object.fromEntries(urlSearchParams.entries());
-                     if(params.page){
-                         urlSearchParams.set('page',ele);
-                     }else{
-                         urlSearchParams.append('page', ele);
-                     }
-                     const newParam= urlSearchParams.toString();
-                     //console.log(newParam);
+			<ul className={styles.index}>
+				<li onClick={() => changePage("-")}>{"<"}</li>
+				{index().map((ele) => {
+					if (componentMount) {
+						const urlSearchParams = new URLSearchParams(window.location.search);
+						const params = Object.fromEntries(urlSearchParams.entries());
+						if (params.page) {
+							urlSearchParams.set("page", ele);
+						} else {
+							urlSearchParams.append("page", ele);
+						}
+						const newParam = urlSearchParams.toString();
+						//console.log(newParam);
 
-                     return(
-                      <li key={ele} style={ele !== actualPage ? {color:'#979797'} : {}}>
-                          <Link href={store ? `/tienda?${newParam}` : `/admin?${newParam}`}>
-                              <a onClick={()=>{
-                                  setActualPage(ele);
-                                  }}>
-                                  {ele}
-                                </a>
-                            </Link>
-                        </li>
-                     )
-                 }
-             }
-             )
-           }
-           <li onClick={()=> changePage('+')}>{'>'}</li>
-       </ul>
+						return (
+							<li key={ele} style={ele !== actualPage ? {color: "#979797"} : {}}>
+								<Link href={store ? `/tienda?${newParam}` : `/admin?${newParam}`}>
+									<a
+										onClick={() => {
+											setActualPage(ele);
+										}}
+									>
+										{ele}
+									</a>
+								</Link>
+							</li>
+						);
+					}
+				})}
+				<li onClick={() => changePage("+")}>{">"}</li>
+			</ul>
 
-       {
-           previewProduct ? 
-           (<Preview product={previewProduct}
-             setPreviewProduct={setPreviewProduct} 
-             currency={currency} 
-             usaToArs={usaToArs}
-             store={store}
-             setEdit={setEdit}
-             setUpdateCart={setUpdateCart}
-             cartProducts={cartProducts}/>):
-           (null)
-       }
-        </div>
-    )
+			{previewProduct ? (
+				<Preview
+					product={previewProduct}
+					setPreviewProduct={setPreviewProduct}
+					currency={currency}
+					usaToArs={usaToArs}
+					store={store}
+					setEdit={setEdit}
+					setUpdateCart={setUpdateCart}
+					cartProducts={cartProducts}
+				/>
+			) : null}
+		</div>
+	);
 }
