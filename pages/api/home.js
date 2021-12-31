27dfff26c92cf198b3dfebfaa0c6bff1618db85a
nicodeheza <nicolasdeheza@hotmail.com>
2021-12-31 13:55:24 -0300

    api/home test logs

--- pages/api/home.js
+++ pages/api/home.js
@@ -9,13 +9,21 @@ handler.use(auth).get(async (req, res) => {
 	try {
 		let usaToArs;
 		const now = new Date();
+		console.log("db exchange query");
 		const dbExchange = await Exchange.find({});
 		if (!dbExchange[0] || dbExchange[0].expires <= now) {
+			console.log("db exchange remove");
+
 			await Exchange.remove({});
+
+			console.log("fetch api data");
 
 			const exchangeRateRes = await fetch(
 				`https://v6.exchangerate-api.com/v6/${process.env.EXCHANGE_RATE_API_KEY}/pair/USD/ARS`
 			);
+
+			console.log("convert data");
+
 			const exchangeRate = await exchangeRateRes.json();
 			usaToArs = exchangeRate.conversion_rate;
 
@@ -32,6 +40,8 @@ handler.use(auth).get(async (req, res) => {
 				exchange: usaToArs,
 				expires: expire
 			});
+			console.log("save exchange in db");
+
 			await newExchange.save();
 			console.log("api");
 		} else {
@@ -40,9 +50,13 @@ handler.use(auth).get(async (req, res) => {
 		}
 
 		if (req.isAuthenticated()) {
+			console.log("get user products");
 			const userProducts = await getUserProducts(req.user.email);
+
+			console.log("res");
 			res.json({auth: true, usaToArs, userProducts});
 		} else {
+			console.log("res");
 			res.json({auth: false, usaToArs, userProducts: []});
 		}
 	} catch (err) {
