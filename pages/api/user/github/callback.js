55cece0dbfafc71eb1b06372d1a0caa65059bda0
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 19:37:56 -0300

    github delete comments

--- pages/api/user/github/callback.js
+++ pages/api/user/github/callback.js
@@ -1,35 +1,11 @@
 import nextConnect from "next-connect";
 import auth from "../../../../middleware/auth";
 import passport from "../../../../lib/passaport";
-//import {createLoginSession} from "../../../../lib/auth";
-//import {serialize} from "cookie";
 
 const handler = nextConnect();
 
-// handler.use(auth).get(passport.authenticate("github"), async (req, res) => {
-// 	//console.log(req.session);
-// 	try {
-// 		const cookieOpts = {
-// 			maxAge: 60 * 60 * 8, // 8hours
-// 			httpOnly: true,
-// 			secure: process.env.NODE_ENV === "production",
-// 			path: "/",
-// 			sameSite: "lax"
-// 		};
-// 		const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
-// 		res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
-// 		res.redirect(307, "/");
-// 		//res.writeHead(302, {Location: "/tienda"}).end();
-// 	} catch (err) {
-// 		res.status(500).send({error: "failed to login with github"});
-// 	}
-// });
 handler.use(auth).get(passport.authenticate("github"), (req, res) => {
-	//res.redirect(307, "/");
 	res.status(200).send(`<script>window.location.href = '/tienda'</script>`);
-	//res.status(200).json({message: "ok"});
-	//res.writeHead(307, {Location: "/tienda"});
-	//res.end();
 });
 
 export default handler;
