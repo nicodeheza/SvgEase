a59745fe0a2cfd2391ba13cdf0b1915b1128b63a
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 19:49:27 -0300

    updating mercado pago back urls

--- pages/api/mp/create_preference.js
+++ pages/api/mp/create_preference.js
@@ -1,43 +1,41 @@
-import mercadoPago from 'mercadopago';
-import nextConnect from 'next-connect';
-import auth from '../../../middleware/auth';
-import authenticated from '../../../middleware/authenticated';
+import mercadoPago from "mercadopago";
+import nextConnect from "next-connect";
+import auth from "../../../middleware/auth";
+import authenticated from "../../../middleware/authenticated";
 
 mercadoPago.configure({
-    access_token: process.env.MP_TEST_ACCESS_TOKEN
-  });
+	access_token: process.env.MP_TEST_ACCESS_TOKEN
+});
 
-  const handler= nextConnect();
+const handler = nextConnect();
 
-  handler
-  .use(auth)
-  .use(authenticated)
-  .post((req, res)=>{
-    const {products}= req.body;
-    
-    //console.log(products);
-    const preference={
-        items: products,
-        back_urls: {
-			"success": "http://localhost:3000/api/mp/feedback",
-			"failure": "http://localhost:3000/api/mp/feedback",
-			"pending": "http://localhost:3000/api/mp/feedback"
-		},
-		auto_return: 'approved',
-    payment_methods:{
-      excluded_payment_types:[
-        {id:'ticket'},
-        {id:'atm'}
-      ],
-      installments: 1
-    }
-    };
+handler
+	.use(auth)
+	.use(authenticated)
+	.post((req, res) => {
+		const {products} = req.body;
 
-    mercadoPago.preferences.create(preference)
-    .then(response =>{
-        res.json({id: response.body.id });
-    })
-    .catch(err=> console.log(err));
-  });
+		//console.log(products);
+		const preference = {
+			items: products,
+			back_urls: {
+				success: "https://svg.nicolasdeheza.com/api/mp/feedback",
+				failure: "https://svg.nicolasdeheza.com/api/mp/feedback",
+				pending: "https://svg.nicolasdeheza.com/api/mp/feedback"
+			},
+			auto_return: "approved",
+			payment_methods: {
+				excluded_payment_types: [{id: "ticket"}, {id: "atm"}],
+				installments: 1
+			}
+		};
+
+		mercadoPago.preferences
+			.create(preference)
+			.then((response) => {
+				res.json({id: response.body.id});
+			})
+			.catch((err) => console.log(err));
+	});
 
 export default handler;
