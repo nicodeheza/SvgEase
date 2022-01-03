ed0850ff63ecf05c657e5f2b26e346d8c2064ac0
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 19:42:10 -0300

    github add comment

--- pages/api/user/github/callback.js
+++ pages/api/user/github/callback.js
@@ -5,6 +5,7 @@ import passport from "../../../../lib/passaport";
 const handler = nextConnect();
 
 handler.use(auth).get(passport.authenticate("github"), (req, res) => {
+	// res.redirect("/tienda") don't work in netlify
 	res.status(200).send(`<script>window.location.href = '/tienda'</script>`);
 });
 
