b15e619ebbe980160987bae600d20de44723864b
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 16:14:10 -0300

    github callback try redirect to index

--- pages/api/user/github/callback.js
+++ pages/api/user/github/callback.js
@@ -6,23 +6,27 @@ import {serialize} from "cookie";
 
 const handler = nextConnect();
 
-handler.use(auth).get(passport.authenticate("github"), async (req, res) => {
-	//console.log(req.session);
-	try {
-		const cookieOpts = {
-			maxAge: 60 * 60 * 8, // 8hours
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			path: "/",
-			sameSite: "lax"
-		};
-		const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
-		res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
-		//res.redirect(307, "/tienda");
-		res.writeHead(302, {Location: "https://svg.nicolasdeheza.com/tienda"}).end();
-	} catch (err) {
-		res.status(500).send({error: "failed to login with github"});
-	}
+// handler.use(auth).get(passport.authenticate("github"), async (req, res) => {
+// 	//console.log(req.session);
+// 	try {
+// 		const cookieOpts = {
+// 			maxAge: 60 * 60 * 8, // 8hours
+// 			httpOnly: true,
+// 			secure: process.env.NODE_ENV === "production",
+// 			path: "/",
+// 			sameSite: "lax"
+// 		};
+// 		const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
+// 		res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
+// 		res.redirect(307, "/");
+// 		//res.writeHead(302, {Location: "/tienda"}).end();
+// 	} catch (err) {
+// 		res.status(500).send({error: "failed to login with github"});
+// 	}
+// });
+handler.use(auth).get(passport.authenticate("github"), (req, res) => {
+	res.redirect(307, "/");
+	//res.writeHead(302, {Location: "/tienda"}).end();
 });
 
 export default handler;
