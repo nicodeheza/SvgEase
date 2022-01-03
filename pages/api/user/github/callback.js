7907008670cfbad3d3a666d49c74240ffb727612
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 14:07:08 -0300

    github callback only redirect

--- pages/api/user/github/callback.js
+++ pages/api/user/github/callback.js
@@ -8,20 +8,20 @@ const handler = nextConnect();
 
 handler.use(auth).get(passport.authenticate("github"), async (req, res) => {
 	//console.log(req.session);
-	try {
-		const cookieOpts = {
-			maxAge: 60 * 60 * 8, // 8hours
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			path: "/",
-			sameSite: "lax"
-		};
-		const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
-		res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
-		res.redirect(307, "/tienda");
-	} catch (err) {
-		res.status(500).send({error: "failed to login with github"});
-	}
+	//try {
+	// const cookieOpts = {
+	// 	maxAge: 60 * 60 * 8, // 8hours
+	// 	httpOnly: true,
+	// 	secure: process.env.NODE_ENV === "production",
+	// 	path: "/",
+	// 	sameSite: "lax"
+	// };
+	// const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
+	// res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
+	res.redirect(307, "/tienda");
+	// } catch (err) {
+	// 	res.status(500).send({error: "failed to login with github"});
+	// }
 });
 
 export default handler;
