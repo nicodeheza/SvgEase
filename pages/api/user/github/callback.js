d309d0aa5ab925173f025cab1a93669d4b0968db
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 15:35:02 -0300

    github callback only  writeHead

--- pages/api/user/github/callback.js
+++ pages/api/user/github/callback.js
@@ -1,28 +1,28 @@
 import nextConnect from "next-connect";
 import auth from "../../../../middleware/auth";
 import passport from "../../../../lib/passaport";
-import {createLoginSession} from "../../../../lib/auth";
-import {serialize} from "cookie";
+//import {createLoginSession} from "../../../../lib/auth";
+//import {serialize} from "cookie";
 
 const handler = nextConnect();
 
 handler.use(auth).get(passport.authenticate("github"), async (req, res) => {
 	//console.log(req.session);
-	try {
-		const cookieOpts = {
-			maxAge: 60 * 60 * 8, // 8hours
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			path: "/",
-			sameSite: "lax"
-		};
-		const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
-		res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
-		//res.redirect(307, "/tienda");
-		res.writeHead(302, {Location: "/tienda"}).end();
-	} catch (err) {
-		res.status(500).send({error: "failed to login with github"});
-	}
+	//try {
+	// const cookieOpts = {
+	// 	maxAge: 60 * 60 * 8, // 8hours
+	// 	httpOnly: true,
+	// 	secure: process.env.NODE_ENV === "production",
+	// 	path: "/",
+	// 	sameSite: "lax"
+	// };
+	//const token = await createLoginSession(req.session, process.env.TOKEN_SECRET);
+	//res.setHeader("Set-Cookie", serialize("sess", token, cookieOpts));
+	//res.redirect(307, "/tienda");
+	res.writeHead(302, {Location: "/tienda"}).end();
+	// } catch (err) {
+	// 	res.status(500).send({error: "failed to login with github"});
+	// }
 });
 
 export default handler;
