430607718de676736563ff45e90dd2a572041e4b
nicodeheza <nicolasdeheza@hotmail.com>
2022-01-03 19:56:54 -0300

    mercado pago redirect

--- pages/api/mp/feedback.js
+++ pages/api/mp/feedback.js
@@ -1,50 +1,51 @@
-import nextConnect from 'next-connect';
-import auth from '../../../middleware/auth';
-import authenticated from '../../../middleware/authenticated';
-import dbConnect from '../../../lib/mongooseConect';
-import User from '../../../models/UserSchema';
+import nextConnect from "next-connect";
+import auth from "../../../middleware/auth";
+import authenticated from "../../../middleware/authenticated";
+import dbConnect from "../../../lib/mongooseConect";
+import User from "../../../models/UserSchema";
 
-const handler= nextConnect();
+const handler = nextConnect();
 
 handler
-.use(auth)
-.use(authenticated)
-.get(async (req, res)=>{
-    // console.log({
-	// 	Payment: req.query.payment_id,
-	// 	Status: req.query.status,
-	// 	MerchantOrder: req.query.merchant_order_id,
-    //     externalReference: req.query.external_reference	
-	// });
-    try {
-        await dbConnect();
-        if(req.query.status === 'approved'){
-        const response= await fetch(`https://api.mercadopago.com/merchant_orders/search?preference_id=${req.query.preference_id}`,{
-            method: 'GET',
-            headers:{
-                'Authorization': `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
-                'Accept': 'application/json',
-            }
-        });
-        const preference = await response.json();
-        //console.log(preference.elements[0].items);
+	.use(auth)
+	.use(authenticated)
+	.get(async (req, res) => {
+		// console.log({
+		// 	Payment: req.query.payment_id,
+		// 	Status: req.query.status,
+		// 	MerchantOrder: req.query.merchant_order_id,
+		//     externalReference: req.query.external_reference
+		// });
+		try {
+			await dbConnect();
+			if (req.query.status === "approved") {
+				const response = await fetch(
+					`https://api.mercadopago.com/merchant_orders/search?preference_id=${req.query.preference_id}`,
+					{
+						method: "GET",
+						headers: {
+							Authorization: `Bearer ${process.env.MP_TEST_ACCESS_TOKEN}`,
+							Accept: "application/json"
+						}
+					}
+				);
+				const preference = await response.json();
+				//console.log(preference.elements[0].items);
 
-        const user= await User.findById(req.user._id);
+				const user = await User.findById(req.user._id);
 
-        preference.elements[0].items.forEach(ele=>{
-            user.userProducts.push(ele.id);
-        });
-            //console.log(userProducts);
-            await user.save();
+				preference.elements[0].items.forEach((ele) => {
+					user.userProducts.push(ele.id);
+				});
+				//console.log(userProducts);
+				await user.save();
+			}
 
-        }
-
-        res.redirect('/tienda');
-    
-        
-    } catch (err) {
-        console.log(err);
-    }
-})
+			//res.redirect('/tienda'); don't work in netlify
+			res.status(200).send(`<script>window.location.href = '/tienda'</script>`);
+		} catch (err) {
+			console.log(err);
+		}
+	});
 
 export default handler;
